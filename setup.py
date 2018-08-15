a7401744d3f1f415cc4fb3e5b73a76179d539a15
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-15 13:10:55 -0400

    setup template

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
 #!/usr/bin/env python
-"""
-conda install gdal cartopy
-"""
-install_requires = ['numpy','cartopy','gdal']
-tests_require=['nose','coveralls']
-
-# %%
-from setuptools import setup, find_packages
-
-setup(name='groundwatertable',
-      packages=find_packages(),
-      version = '0.1.0',
-      description='Plotting of groundwater table using .shp shape files.',
-      author = 'Michael Hirsch, Ph.D.',
-      url = 'https://github.com/scivision/groundwatertable',
-      classifiers=[
-      'Intended Audience :: Science/Research',
-      'Development Status :: 3 - Alpha',
-      'License :: OSI Approved :: MIT License',
-      'Topic :: Scientific/Engineering :: GIS',
-      'Programming Language :: Python :: 3',
-      ],
-      install_requires=install_requires,
-      extras_require={'plot':['matplotlib'],
-                      'tests':tests_require,},
-      tests_require=tests_require,
-      python_requires='>=3.5',
-	  )
-
+from setuptools import setup
+setup()
