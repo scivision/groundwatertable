a309f7784a9f4335aa104d5fbfc3194f5dddcf47
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-15 13:12:12 -0400

    pep8

--- PlotWatertable.py
+++ PlotWatertable.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 from matplotlib.pyplot import show
 from groundwatertable import waterplot
+from argparse import ArgumentParser
 
-if __name__ == '__main__':
-    from argparse import ArgumentParser
+
+def main():
     p = ArgumentParser()
-    p.add_argument('datdir',help='directory to .adf file')
-    p.add_argument('shpfn',help='.shp file to overlay, points and text')
+    p.add_argument('datdir', help='directory to .adf file')
+    p.add_argument('shpfn', help='.shp file to overlay, points and text')
     p = p.parse_args()
 
     waterplot(p.datdir, p.shpfn)
 
     show()
+
+
+if __name__ == '__main__':
+    main()
--- groundwatertable/__init__.py
+++ groundwatertable/__init__.py
@@ -1,10 +1,10 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 from matplotlib.pyplot import figure
 import cartopy
-from osgeo import osr,gdal
+from osgeo import osr, gdal
 
 
-def waterplot(datfn,shpfn):#,mlat,mlon):
+def waterplot(datfn, shpfn):  # ,mlat,mlon):
     """
     example;
     https://ocefpaf.github.io/python4oceanographers/blog/2015/03/02/geotiff/
@@ -14,9 +14,9 @@ def waterplot(datfn,shpfn):#,mlat,mlon):
     if ds is None:
         raise OSError(f'could not read {datfn}')
 
-    wt = ds.ReadAsArray() # numpy ndarray
+    wt = ds.ReadAsArray()  # numpy ndarray
 
-    gt = ds.GetGeoTransform()
+    # gt = ds.GetGeoTransform()
     proj = ds.GetProjection()
 
     inproj = osr.SpatialReference()
@@ -26,8 +26,4 @@ def waterplot(datfn,shpfn):#,mlat,mlon):
 #    projcs = inproj.GetAuthorityCode('LOCAL_CS')
 
     ax = figure().gca(projection=cartopy.crs.Mercator())
-    ax.imshow(wt,origin='lower')
-
-
-
-
+    ax.imshow(wt, origin='lower')
